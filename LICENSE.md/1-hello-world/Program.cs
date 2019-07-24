0b227d1ca4bb98cc44132efc9f708c5e8008f524
Captain Storm <CaptainStorm21@users.noreply.github.com>
2019-07-24 03:32:41 -0700

    hello world
    go to prompt and add dotnet run -- whatever-your-name-is

---
+++ LICENSE.md/1-hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write($"Good morning, {args[0]}!");
+          
+        }
+    }
+}
