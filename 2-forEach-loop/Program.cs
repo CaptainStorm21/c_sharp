ad43aad39376907b67f6403fb968185d552038b5
Captain Storm <CaptainStorm21@users.noreply.github.com>
2019-07-25 23:25:03 -0700

    Add files via upload

---
+++ 2-forEach-loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var numbers = new double [] {12, 23.4, 21.11, 233, 900};
+            var grades = new List <double>()  {12, 23.4, 21.11, 233, 900};
+            grades.Add(90010000);
+            
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            System.Console.WriteLine(result);
+        }
+    }
+}
