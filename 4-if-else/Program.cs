a86b35ac5e7a8d53bcadfc16aef999e2d7c37917
Captain Storm <CaptainStorm21@users.noreply.github.com>
2019-07-25 23:36:14 -0700

    Add files via upload

---
+++ 4-if-else/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var numbers = new double [] {12, 23.4, 21.11, 233, 900};
+            var grades = new List <double>()  {12, 23.4, 21.11, 233, 900};
+            grades.Add(90010000);
+            
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            //to calculate an average
+            result = result/grades.Count;
+            System.Console.WriteLine($"Hello, the average is {result}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello from {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("Hello! This is invalid");
+            }
+        }
+    }
+}
